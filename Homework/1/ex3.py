4ce4953e2e813d4bb305ed8e9d154a6bc8a3c892
Carlos <karlossanpa@gmail.com>
2019-02-10 17:27:13 +0100

    Reformatted code

--- Homework/1/ex3.py
+++ Homework/1/ex3.py
@@ -2,9 +2,9 @@ from genetic_algorithm import *
 from parameters import *
 
 
-#Show the grid
+# Show the grid
 print('\n'.join([''.join(['{:4}'.format(item) for item in row])
-      for row in grid]))
+                 for row in grid]))
 
 
 population = get_population()
@@ -15,18 +15,19 @@ for i in range(generations):
     population = selection_and_reproduction(population)
     population = mutation(population)
 
-best_individual=population[0]
-best_fitness=get_fitness(best_individual)
+best_individual = population[0]
+best_fitness = get_fitness(best_individual)
 
 if best_fitness == NOT_SUITABLE:
     print("The genetic algorithm could not find a solution.")
     exit(-1)
 else:
-    print("Best solution: {} -> {} -> fitness = {}".format(best_individual,get_phenotype(best_individual),get_fitness(population[0])))
+    print("Best solution: {} -> {} -> fitness = {}".format(best_individual,
+                                                           get_phenotype(best_individual), best_fitness))
 
     print("Final population (phenotype):")
     for i in population:
         print(get_phenotype(i))
     print("Fitnesses:")
     for i in population:
-        print("{} ->{}".format(i,get_fitness(i)))
+        print("{} ->{}".format(i, get_fitness(i)))
