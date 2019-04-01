45d3b10ae023c56b7729428d481db8b89e048a4a
Carlos <karlossanpa@gmail.com>
2019-04-01 19:16:03 +0200

    Finished genetic algorithm

--- Homework/6/ex4.py
+++ Homework/6/ex4.py
@@ -1,14 +1,21 @@
 from tsp import TSP
+from math import inf
 
 generations = 10
-population_size = 3
+population_size = 4
 mutation_chance = 0.2
-adj_matrix = [1, 0, 0], [0, 1, 0], [0, 0, 1]
+adj_matrix = [[inf, 10, 8, 3],
+              [10, inf, 4, 2],
+              [8, 4, inf, 1],
+              [3, 2, 1, inf]]
 
 
 instance = TSP(adj_matrix, generations, population_size, mutation_chance)
 instance.genetic_algorithm()
 population = instance.get_population()
+
+print("Number of generations: {}\nSize of the population:{}\nMutation chance:{}".format(
+    generations, population_size, mutation_chance))
 
 print("The best individual is {} and its fitness is {}".format(
     population[0], instance.get_fitness(population[0])))
--- Homework/6/tsp.py
+++ Homework/6/tsp.py
@@ -1,4 +1,5 @@
 from random import shuffle, randint, random
+from math import inf
 
 
 class TSP:
@@ -35,20 +36,62 @@ class TSP:
 
     def mutation(self):
         if random() <= self.mutation_chance:
-            position = randint(0, len(self.population))
+            position = randint(0, len(self.population) - 1)
             self.population[position] = self.inversion_mutation(
                 self.population[position])
 
+    def create_neighbours_lists(self):
+        lists = [[0 for x in range(self.cities)] for y in range(self.cities)]
+        for city in range(self.cities):
+            for neighbour in range(self.cities):
+                if self.adj_matrix[city][neighbour] != inf and city != neighbour:
+                    lists[city].append(neighbour)
+        return lists
+
+    def remove_from_lists(self, element, lists):
+        for i in range(1, len(lists)):
+            if element in lists[i]:
+                lists[i].remove(element)
+        return lists
+
+    def random_node(self, lists, child):
+        correct = False
+        while not correct:
+            node = randint(self.cities)
+            if node not in child:
+                correct = True
+        return node
+
+    def fewest_neighbours(self, lists, current):
+        min = inf
+        result = None
+        for i in lists[current]:
+            if len(lists[i]) < min:
+                min = len(lists[i])
+                result = lists[i]
+        return result[0]
+
     def ex_crossover(self, a, b):
+        lists = self.create_neighbours_lists()
+        chosen_parent = a if random() % 2 == 0 else b
+        current = chosen_parent[0]
         child = []
+        while len(child) != self.cities:
+            child.append(current)
+            lists = self.remove_from_lists(current, lists)
+            if len(lists[current]) == 1:
+                new = self.random_node(lists, child)
+            else:
+                new = self.fewest_neighbours(lists, current)
+            current = new
         return child
 
     def get_fitness(self, individual):
         fitness = 0
-        for i in range(self.cities - 2):
-            fitness += self.adj_matrix[i][i + 1]
+        for i in range(self.cities - 1):
+            fitness += self.adj_matrix[individual[i]][individual[i + 1]]
         # Back to the first city
-        fitness += self.adj_matrix[0][self.cities - 1]
+        fitness += self.adj_matrix[individual[0]][individual[self.cities - 1]]
         return fitness
 
     # Sort according to the fitness
@@ -64,7 +107,7 @@ class TSP:
         self.population.append(new_individual)
 
     def genetic_algorithm(self):
-        print(self.population)
+        print("Initial population: {}".format(self.population))
         for i in range(self.generations):
             self.selection_and_reproduction()
             self.mutation()
